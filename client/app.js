4390efe697de8b2c3de47080788a68f5fa1a69e4
owensmyth80 <owensmyth80@gmail.com>
2023-12-06 07:45:00 +0000

    client side update
    created client side and populated with some info

--- client/app.js
+++ client/app.js
@@ -7,9 +7,9 @@ var packageDefinition = protoLoader.loadSync(PROTO_PATH)
 var lights = grpc.loadPackageDefinition(packageDefinition).lights;
 var client = new lights.LightReg("0.0.0.0:40000", grpc.credentials.createInsecure());
 //fix ID to id
-client.RegStreetLight({ streetLightId: "1", streetLightName: "Light 1", streetLightLat: 53.34889, streetLightLong: -6.31336 }, handleResponse);
-client.RegStreetLight({ streetLightId: "2", streetLightName: "Light 2", streetLightLat: 53.35027, streetLightLong: -6.31530 }, handleResponse);
-client.RegStreetLight({ streetLightId: "3", streetLightName: "Light 3", streetLightLat: 53.35233, streetLightLong: -6.32303 }, handleResponse);
+client.RegStreetLight({ streetLightId: "LIGHT0001", streetLightName: "Park Light 1", streetLightZone: 1, streetLightLat: 53.34889, streetLightLong: -6.31336 }, handleResponse);
+client.RegStreetLight({ streetLightId: "LIGHT0002", streetLightName: "Park Light 2", streetLightZone: 2, streetLightLat: 53.35027, streetLightLong: -6.31530 }, handleResponse);
+client.RegStreetLight({ streetLightId: "LIGHT0003", streetLightName: "Park Light 3", streetLightZone: 3, streetLightLat: 53.35233, streetLightLong: -6.32303 }, handleResponse);
 
 function handleResponse(error, response) {
     if (error) {
--- server/app.js
+++ server/app.js
@@ -11,19 +11,28 @@ var server = new grpc.Server()
 
 server.addService(proto.LightReg.service, {
   RegStreetLight: (call, callback) => {
-    const { streetLightId, streetLightName, streetLightLat, streetLightLong } = call.request;
+    const { streetLightId, streetLightName, streetLightZone, streetLightLat, streetLightLong } = call.request;
 
-    lights[streetLightId] = { streetLightName, streetLightLat, streetLightLong, is_on: false };
+    lights[streetLightId] = { streetLightName, streetLightZone, streetLightLat, streetLightLong, is_on: false };
 
     console.log(`Registered Streetlight: ${streetLightName} (${streetLightId})`);
+
+    callback(null, { streetLightRegStatus: true, message: "Light Registered" });
     //console log all details to confirm they exist
     console.log("The lights are ", lights);
-    callback(null, { streetLightRegStatus: true, message: "Light Registered" });
-    
   },
+
+
 });
 
-
+  //checking the var light stays after the service is called
+  if(Object.keys(lights).length > 0){
+    console.lgog("the lights in the var lights are ", lights);
+  }
+  else{
+    console.log("nothing in the var lights");
+    
+  }
 
 
 server.bindAsync("0.0.0.0:40000", grpc.ServerCredentials.createInsecure(), function() {
